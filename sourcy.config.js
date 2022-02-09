a631206c26d20c15a22fa9bc02db6e6ab88d5182
Aleksey Razbakov <aleksey@razbakov.com>
2022-02-09 02:06:43 +0100

    cleanup

--- sourcy.config.js
+++ sourcy.config.js
@@ -6,11 +6,4 @@ module.exports = {
       output: '../events-database/events/spreadsheet_festivals',
       format: 'yaml',
     },
-    {
-      spreadsheetId: '1HipCCapgfMJIqkLpckmkTXToJJd7cKSOjPFTNplkP3g',
-      range: 'Events',
-      output: '../events-database/events/spreadsheet_festivals',
-      format: 'yaml',
-    },
-  ],
 }

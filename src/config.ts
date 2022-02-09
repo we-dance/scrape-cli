f9e0e41a16cf8ee9980d37d5b2019889648297ba
Aleksey Razbakov <aleksey@razbakov.com>
2022-02-09 02:01:23 +0100

    cleanup

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,6 @@ import 'dotenv/config'
 
 const config = {
   eventsDatabase: `${process.env.APP_EVENTS_DATABASE}`,
-  usersDatabase: `${process.env.APP_USER_DATABASE}`,
   verbose: 0,
   retry: false,
   silent: false,
